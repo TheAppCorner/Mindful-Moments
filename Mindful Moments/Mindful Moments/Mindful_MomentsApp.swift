ab3206dbbba5a60d1adcbebbf81befe93eca2090
The App Corner <theappcorner@M3-MacBook-Pro.local>
2024-02-11 14:36:22 -0800

    First commit

---
+++ Mindful Moments/Mindful Moments/Mindful_MomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mindful_MomentsApp.swift
+//  Mindful Moments
+//
+//  Created by The App Corner on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Mindful_MomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
